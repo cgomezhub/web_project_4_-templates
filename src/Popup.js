bbf2a6d385a037b5b34339a8b09565a9b460306c
Carlos Gomez <cgomezlugo@hotmail.com>
2023-09-27 06:32:18 -0400

    features: set class Popup

---
+++ src/Popup.js
@@ -0,0 +1,37 @@
+
+
+export default class Popup {
+	constructor(popupSelector) {
+    this._popupSelector = popupSelector;
+	}
+
+  
+    open() {
+        popupSelector.classList.add('active');
+    }
+      
+    close() {
+        popupSelector.classList.remove('active');
+    }
+
+    _handleEscClose(event) {
+        if (event.target === "escape") {
+          close();
+        }
+    }
+
+
+	setEventListeners() {
+		this._element.querySelector(".form__close").addEventListener("click", () => {
+			this.close();
+		});
+
+        this._element.querySelector(".popup").addEventListener("click", () => {
+			this.close();
+		});
+        /*document.addEventListener('keydown', (event) =>{
+            this._handleEscClose(event);            
+          });*/
+    }
+
+}
